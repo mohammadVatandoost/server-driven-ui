07c5a42409c18150d3e5e91763924cd02342075c
mohammadvatandoost <mohamadvatandoost512@gmail.com>
2022-07-21 21:58:34 +0200

    complete UI

--- UI/src/App.js
+++ UI/src/App.js
@@ -1,17 +1,11 @@
 import React,{useState,useEffect} from 'react';
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NewTable from './components/NewTable'
-import ImageComponent from './components/ImageComponent'
-import { Table } from 'react-bootstrap';
 
 function App() {
-  const [data, setData] = useState();
   const [output, setOutput] = useState();
-
-  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,43 +15,30 @@ function App() {
       console.log('data:',results)
       let result = []
       results.data.payload.map((item) => {
-        if(item.Type == "Title") {
+        if(item.Type === "Title") {
           result.push(<h1>{item.Data.Title}</h1>)
         } 
-        else if(item.Type == "Image") {
+        else if(item.Type === "Image") {
           result.push(<img src={item.Data.Source} />)
         } 
-        else if(item.Type == "Description") {
+        else if(item.Type === "Description") {
           result.push(<p>{item.Data.Description}</p>)
         } 
-        else if(item.Type == "Title") {
-          result.push(<NewTable table={item.Data.Titles}/>)
+        else if(item.Type === "Table") {
+          result.push(<NewTable table={item.Data}/>)
         } 
         
       })
       setOutput(result)
-      console.log('result:',result)
-      // setData(results.data);
-      // const imageData =data.payload[2]
-      // console.log(imageData)
     };
 
     fetchData();
     
-
   }, []);
- 
-
 
   return (
    <div className='container'>
     {output}
-    {/* <h1>Demo Page</h1>
-    <ImageComponent />
-    <div className='dataList'>
-      <h2></h2>
-      <NewTable/>
-    </div> */}
    </div>
   );
 }
--- UI/src/components/NewTable.js
+++ UI/src/components/NewTable.js
@@ -1,39 +1,39 @@
 import React from "react";
 import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { NavItem } from "react-bootstrap";
 
 
 function NewTable(table){
+    
+    const Titles = () => {
+      return table.table.Titles.map( (item)=> {
+        return (
+       
+          <th>{item}</th>
+       
+        )
+      })
+    }
+
+    const Rows = () => {
+      return table.table.Rows.map( (items)=> {
+        let tmp = items.map((item)=>{
+          return (
+               <td>{item}</td> 
+            )
+          })
+        return (
+          <tr> {tmp}</tr>
+        ) 
+       
+      })
+    }
 
     return(
     <Table striped bordered hover>
-        table.map((item)=>(
-          <tr>
-            <td>{item.Titles}</td>
-          </tr>
-        ))
-       
-        <tr>
-          <td>col1</td>
-          <td>col1</td>
-          <td>col1</td>
-          <td>col1</td>
-        </tr>
-        <tr>
-          <td>col1</td>
-          <td>col1</td>
-          <td>col1</td>
-          <td>col1</td>
-        </tr>
-        <tr>
-          <td>col1</td>
-          <td>col1</td>
-          <td>col1</td>
-          <td>col1</td>
-        </tr>
-        
-      </Table>)
+      <tr>{Titles()}</tr>
+      {Rows()}
+    </Table>)
 }
 
 export default NewTable;
